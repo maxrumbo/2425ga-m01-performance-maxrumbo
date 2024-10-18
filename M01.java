94c96a6c3477de6c422e4c9f9e5b7fa4b9408ef4
Maxwell Rumahorbo <maxrumbo06@gmail.com>
2024-10-18 10:20:18 -0700

    PAIR 20

--- M01.java
+++ M01.java
@@ -1,2 +1,58 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24037 - Maxwell Rumahorbo
+// 12S24030 - Swasti Sihombing
+
+import java.util.*;
+import java.lang.Math;
+
+public class M01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String kredit, nilaihuruf;
+        double nilaiangka, performa;
+
+        kredit = input.nextLine();
+        while (!kredit.equals("---")) {
+            if (kredit.equals("---")) {
+                kredit = kredit;
+            } else {
+                nilaiangka = Integer.parseDouble(kredit);
+            }
+            nilaihuruf = input.nextLine();
+            if (nilaihuruf.equals("A")) {
+                nilaiangka = 4.0;
+            } else {
+                if (nilaihuruf.equals("AB")) {
+                    nilaiangka = 3.5;
+                } else {
+                    if (nilaihuruf.equals("B")) {
+                        nilaiangka = 3.0;
+                    } else {
+                        if (nilaihuruf.equals("BC")) {
+                            nilaiangka = 2.5;
+                        } else {
+                            if (nilaihuruf.equals("C")) {
+                                nilaiangka = 2.0;
+                            } else {
+                                if (nilaihuruf.equals("D")) {
+                                    nilaiangka = 1.0;
+                                } else {
+                                    if (nilaihuruf.equals("E")) {
+                                        nilaiangka = 0.0;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            performa = Integer.parseDouble(kredit) * nilaiangka;
+            System.out.println(toFixed(performa,1));
+            kredit = input.nextLine();
+        }
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
